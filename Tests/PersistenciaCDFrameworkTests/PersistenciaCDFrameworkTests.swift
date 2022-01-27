fff49da6e52d9c887f688e09ecc9d71fa32bdceb
Juan Hernandez <jl_hl@hotmail.com>
2022-01-27 13:03:15 -0600

    Pruebas

--- Tests/PersistenciaCDFrameworkTests/PersistenciaCDFrameworkTests.swift
+++ Tests/PersistenciaCDFrameworkTests/PersistenciaCDFrameworkTests.swift
@@ -6,6 +6,29 @@ final class PersistenciaCDFrameworkTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(PersistenciaCDFramework().text, "Hello, World!")
+        //XCTAssertEqual(PersistenciaCDFramework().text, "Hello, World!")
+    }
+    
+    func testGuardarPeli () {
+        let manejador = PersistenciaCDFramework()
+        let titulo = "Club de la pelea"
+        
+        manejador.nuevaPelicula(id: "550", titulo: titulo, sinopsis: "Una pelicula con Brad Pitt", lanzamiento: "2000")
+        let pelis = manejador.obtenerPeliculas()
+        
+        XCTAssertEqual(pelis[0].titulo, titulo)
+    }
+    
+    func testMovie () {
+        let peli = Movie(id: "1", titulo: "Club de la Pelea", sinopsis: "Pelicula con Brad Pitt", lanz: "2000")
+        XCTAssertNotNil(peli)
+        XCTAssertEqual(peli.titulo, "Club de la Pelea")
+    }
+    
+    func testTvSerie () {
+        let serie = TvSerie(id: "1", titulo: "Dexter", sinopsis: "Trata de un asesino serial", episodios: "60", temporadas: "7", lanz: "2002")
+        XCTAssertNotNil(serie)
+        XCTAssertEqual(serie.titulo, "Dexter")
+        
     }
 }
