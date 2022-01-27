267c153bd92103aac751980886df3190edce06e6
Juan Hernandez <jl_hl@hotmail.com>
2022-01-27 13:03:10 -0600

    Structs de datos

---
+++ Sources/PersistenciaCDFramework/Movie.swift
@@ -0,0 +1,32 @@
+//
+//  Movie.swift
+//  MoviedbFramework
+//
+//  Created by temp on 26/01/22.
+//
+
+import Foundation
+
+public struct Movies: Decodable {
+    let status: String
+    public let data: [Movie]
+}
+
+public struct Movie: Decodable {
+    public let id, titulo, sinopsis, lanzamiento: String
+    public var imagen:String?
+    
+    public init(id: String, titulo: String, sinopsis: String, lanz: String) {
+        self.id = id
+        self.titulo = titulo
+        self.sinopsis = sinopsis
+        self.lanzamiento = lanz
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case titulo = "title"
+        case sinopsis = "overview"
+        case lanzamiento = "release_date"
+    }
+}
---
+++ Sources/PersistenciaCDFramework/TvSerie.swift
@@ -0,0 +1,37 @@
+//
+//  TvSerie.swift
+//  MoviedbFramework
+//
+//  Created by temp on 26/01/22.
+//
+
+import Foundation
+
+public struct TvSeries: Decodable {
+    let status: String
+    public let data: [TvSerie]
+}
+
+public struct TvSerie: Decodable {
+    public let id, titulo, sinopsis, episodios, temporadas, lanzamiento: String
+    public var imagen:String?
+    
+    public init(id: String, titulo: String, sinopsis: String, episodios: String, temporadas: String, lanz: String) {
+        self.id = id
+        self.titulo = titulo
+        self.sinopsis = sinopsis
+        self.episodios = episodios
+        self.temporadas = temporadas
+        self.lanzamiento = lanz
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case titulo = "name"
+        case sinopsis = "overview"
+        case lanzamiento = "first_air_date"
+        case episodios = "number_of_episodes"
+        case temporadas = "number_of_seasons"
+    }
+    
+}
