1c1b6f12eaf09f6a065380678b82dce04a5f0002
Juan Hernandez <jl_hl@hotmail.com>
2022-01-27 19:49:08 -0600

    Corrección struct tvseries

--- Sources/PersistenciaCDFramework/TvSerie.swift
+++ Sources/PersistenciaCDFramework/TvSerie.swift
@@ -20,26 +20,24 @@ public struct TvSeries: Decodable {
 }
 
 public struct TvSerie: Decodable {
-    public let titulo, sinopsis, episodios, temporadas, lanzamiento: String
-    public let id: Int
+    public let titulo, sinopsis, lanzamiento: String
+    public let id, votacion: Int
     public var imagen:String?
     
-    public init(id: Int, titulo: String, sinopsis: String, episodios: String, temporadas: String, lanz: String) {
+    public init(id: Int, titulo: String, sinopsis: String, lanz: String, votos: Int) {
         self.id = id
         self.titulo = titulo
         self.sinopsis = sinopsis
-        self.episodios = episodios
-        self.temporadas = temporadas
         self.lanzamiento = lanz
+        self.votacion = votos
     }
     
     enum CodingKeys: String, CodingKey {
         case id
+        case votacion = "vote_average"
         case titulo = "name"
         case sinopsis = "overview"
         case lanzamiento = "first_air_date"
-        case episodios = "number_of_episodes"
-        case temporadas = "number_of_seasons"
         case imagen = "poster_path"
     }
     
